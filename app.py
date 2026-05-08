2ed61cb519a767990d43ed134be4318c7f28a8e3
savarni-new <gr.varnika012@gmail.com>
2026-05-07 22:26:29 -0400

    Created app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+# Simple Machine Learning Example
+
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+# Training data
+X = np.array([[1], [2], [3], [4], [5]])
+y = np.array([2, 4, 6, 8, 10])
+
+# Create model
+model = LinearRegression()
+
+# Train model
+model.fit(X, y)
+
+# Predict value
+prediction = model.predict([[6]])
+
+# Print result
+print("Prediction for 6 is:", prediction[0])
